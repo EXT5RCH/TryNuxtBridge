8bfda530c39260aad565b39bd35d957e2ff1ea1c
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-07-16 23:26:10 +0900

    [feat]`VtAccordion`作成

---
+++ components/uiParts/VAccordion/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  label: string;
+  open?: boolean;
+}
+withDefaults(defineProps<Props>(), {
+  label: "",
+  open: true,
+});
+</script>
+
+<template>
+  <details :open="open" class="v-accordion">
+    <summary v-text="label" />
+    <slot />
+  </details>
+</template>
+
+<style lang="postcss">
+.v-accordion {
+  @apply border;
+  @apply rounded;
+}
+</style>
--- components/uiParts/index.ts
+++ components/uiParts/index.ts
@@ -1,4 +1,5 @@
+import VAccordion from "./VAccordion/index.vue";
 import VButton from "./VButton/index.vue";
 import VTextbox from "./VTextbox/index.vue";
 
-export { VButton, VTextbox };
+export { VAccordion, VButton, VTextbox };
