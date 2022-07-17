a925ea5f446953b77d8f11249ef5376551b7a8b5
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-07-17 13:31:56 +0900

    [feat]`layout`作成

---
+++ layouts/user.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="layout">
+    <div class="header">ヘッダー</div>
+    <div class="body">
+      <Nuxt />
+    </div>
+    <div class="footer">フッター</div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.layout {
+  @apply h-full w-full;
+  .header {
+    @apply h-6;
+    @apply bg-white;
+    @apply flex;
+    @apply justify-center items-center;
+  }
+
+  .body {
+    @apply flex;
+    height: calc(100% - 3rem);
+  }
+
+  .footer {
+    @apply bottom-0;
+    @apply flex;
+    @apply justify-center items-center;
+    @apply h-6;
+    @apply w-full;
+    @apply bg-gray-900;
+    @apply text-gray-200;
+  }
+}
+</style>
--- pages/news.vue
+++ pages/news.vue
@@ -1,3 +1,9 @@
+<script lang="ts">
+export default {
+  layout: "user",
+};
+</script>
+
 <script setup lang="ts">
 import NewsTemplate from "@/components/pages/news/NewsTemplate/index.vue";
 </script>
