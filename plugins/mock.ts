b5167005f4bc038ebdf9e6b7ff16df17740cb226
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-07-30 20:00:38 +0900

    [fix]require⇒import

--- plugins/mock.ts
+++ plugins/mock.ts
@@ -1,6 +1,7 @@
+import { worker } from "../mocks/browser.js";
+
 export default () => {
   if (process.env.NODE_ENV === "development") {
-    const { worker } = require("../mocks/browser.js");
     worker.start();
   }
 };
