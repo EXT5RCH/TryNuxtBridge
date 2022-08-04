4a0103411836901a1d37148ba811b0f1a2945b30
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-08-04 23:36:34 +0900

    [update]`VProfileIcon`をアイコン部分だけに変更

--- components/v/profileIcon.vue
+++ components/v/profileIcon.vue
@@ -9,39 +9,18 @@ const vbText = props.text.length > 0 ? props.text.substring(0, 1) : 'あ'
 </script>
 
 <template>
-  <div class="v-profile-icon">
-    <button class="v-profile-icon__content">
-      <div class="profile-icon">
-        <span v-text="vbText" />
-      </div>
-      <span class="material-icons">arrow_drop_down</span>
-    </button>
+  <div class="profile-icon">
+    <span v-text="vbText" />
   </div>
 </template>
 
 <style lang="postcss">
-.v-profile-icon {
+.profile-icon {
   @apply flex;
-  @apply items-center;
-  @apply p-0.5;
-  @apply h-12;
-  @apply w-full;
-
-  &__content {
-    @apply p-1;
-    @apply flex;
-    @apply h-full;
-    @apply w-full;
-    @apply items-center;
-
-    .profile-icon {
-      @apply flex;
-      @apply justify-center items-center;
-      @apply h-9 w-9;
-      @apply rounded-full;
-      @apply bg-white;
-      @apply border;
-    }
-  }
+  @apply justify-center items-center;
+  @apply h-9 w-9;
+  @apply rounded-full;
+  @apply bg-white;
+  @apply border;
 }
 </style>
