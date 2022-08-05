0ddc2b15b01ff2c60826eac3bc568cb61a218504
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-08-05 23:36:46 +0900

    feat: create `VPassword`

---
+++ components/v/Password.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+interface Props {
+  value?: string
+  label: string
+  id: string
+  disabled?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: '',
+  label: '',
+  id: '',
+  type: 'text',
+  disabled: false,
+})
+
+interface Emits {
+  (e: 'input', newValue: string): void
+}
+
+const emit = defineEmits<Emits>()
+
+const type = ref('password')
+const open = ref(false)
+
+const vmValue = computed({
+  get: () => {
+    return props.value
+  },
+  set: (newValue: string) => {
+    emit('input', newValue)
+  },
+})
+
+const handleViewPassword = () => {
+  open.value = !open.value
+  type.value = open.value ? 'type' : 'password'
+}
+</script>
+
+<template>
+  <label class="v-password">
+    <span class="v-password__label" v-text="label" />
+    <div :disabled="disabled" class="v-password__content">
+      <input
+        :id="id"
+        v-model="vmValue"
+        :type="type"
+        :disabled="disabled"
+        class="v-password__content-input"
+      />
+      <button
+        class="v-password__content-view-icon"
+        :disabled="disabled"
+        type="button"
+        @click="handleViewPassword"
+      >
+        <span v-if="open" class="material-icons"> visibility </span>
+        <span v-else class="material-icons"> visibility_off </span>
+      </button>
+    </div>
+  </label>
+</template>
+
+<style lang="postcss" scoped>
+.v-password {
+  @apply flex flex-col;
+
+  &__content {
+    @apply flex;
+    @apply flex-grow;
+    @apply gap-2;
+    @apply px-3 py-2;
+    @apply border rounded;
+    @apply bg-white;
+
+    &:focus-within {
+      @apply outline-none;
+      @apply border-blue-400;
+    }
+
+    &[disabled] {
+      @apply bg-gray-50;
+      @apply text-gray-500;
+    }
+
+    &-input {
+      @apply flex;
+      @apply flex-grow;
+      @apply border-none;
+
+      &:enabled:focus {
+        @apply outline-none;
+      }
+
+      &:disabled {
+        @apply bg-gray-50;
+        @apply text-gray-500;
+      }
+    }
+
+    &-view-icon {
+      @apply flex items-center;
+
+      &:disabled {
+        @apply cursor-default;
+      }
+    }
+  }
+}
+</style>
