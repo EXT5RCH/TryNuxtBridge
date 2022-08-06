2fa796f396f53f798133fc49a29e702a0d1b2b39
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-08-07 00:13:25 +0900

    refactor: writingを管理様に変更

--- components/writing/Index.vue
+++ components/writing/Index.vue
@@ -10,15 +10,38 @@ const handleChangeTree = (items: any) => {
 <template>
   <div class="writing">
     <div class="writing__menu">
-      <div class="writing__menu-title">記事一覧</div>
+      <div class="writing__menu-list">
+        <div class="title">記事一覧</div>
+        <div class="content">
+          <v-tree :items="categoriesNewsState" @change="handleChangeTree" />
+        </div>
+      </div>
       <div class="writing__menu-new">
         <v-button id="new" class="writing__menu-new-button" text="新規作成" />
       </div>
-      <div class="writing__menu-content">
-        <v-tree :items="categoriesNewsState" @change="handleChangeTree" />
+    </div>
+    <div class="writing__content">
+      <div class="writing__content-editor">
+        <div class="writing__content-editor-header">
+          <div class="title">記事作成</div>
+          <div class="buttons">
+            <v-button text="下書き" mode="secondary" />
+            <v-button text="公開" />
+          </div>
+        </div>
+        <div class="writing__content-editor-body">
+          <v-text-field label="タイトル" />
+          <div>
+            <div>本文</div>
+            <div>リッチエディタを配置</div>
+          </div>
+          <div class="section">
+            <div>画像</div>
+            <div>画像アップロードのコンポーネントを配置</div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="writing__editor" />
   </div>
 </template>
 
@@ -27,30 +50,40 @@ const handleChangeTree = (items: any) => {
   @apply flex;
   @apply h-full w-full;
   @apply bg-blue-50;
+  @apply p-4;
+  @apply gap-4;
 
   &__menu {
-    @apply p-2;
-    @apply h-full w-60;
-    @apply border-t;
-    @apply border-r;
-    @apply border-b;
-    @apply bg-gray-100;
+    @apply flex flex-col;
+    @apply h-full w-96;
+    @apply gap-4;
 
-    &-title {
-      @apply w-full;
-      @apply flex;
-      @apply justify-center;
-      @apply bg-blue-500;
-      @apply border-2;
-      @apply border-gray-200;
-      @apply font-bold;
-      @apply text-gray-50;
-      @apply py-1;
+    &-list {
+      @apply flex flex-col;
+      @apply flex-grow;
+
+      .title {
+        @apply w-full;
+        @apply flex;
+        @apply justify-center;
+        @apply text-lg;
+        @apply bg-gradient-to-r;
+        @apply from-blue-500 to-purple-500;
+        @apply text-gray-100;
+        @apply py-2;
+      }
+
+      .content {
+        @apply flex-grow;
+        @apply bg-white;
+        @apply shadow;
+        @apply p-1;
+        @apply overflow-scroll;
+      }
     }
 
     &-new {
       @apply flex;
-      @apply py-2;
 
       &-button {
         @apply flex-grow;
@@ -60,35 +93,47 @@ const handleChangeTree = (items: any) => {
         @apply shadow;
       }
     }
-
-    &-selector {
-      @apply flex;
-      @apply py-2;
-      @apply gap-2;
-
-      &-item {
-        @apply flex-grow;
-        @apply flex;
-        @apply justify-center;
-        @apply bg-gray-50;
-        @apply shadow;
-      }
-    }
-
-    &-content {
-      @apply w-full;
-      @apply bg-white;
-      height: calc(100% - 5.75rem - 4px);
-      @apply shadow;
-      @apply p-1;
-      @apply overflow-scroll;
-    }
   }
 
-  &__editor {
-    @apply h-full w-0;
-    @apply flex-col;
-    @apply flex flex-col;
+  &__content {
+    @apply flex;
+    @apply flex-grow;
+
+    &-editor {
+      @apply flex-grow;
+      @apply bg-gray-50;
+      @apply border;
+      @apply border-gray-300;
+      @apply flex flex-col;
+      @apply px-4 py-2;
+      @apply shadow;
+
+      &-header {
+        @apply h-16;
+        @apply flex-grow-0;
+        @apply flex;
+        @apply justify-between items-center;
+        @apply border-b-2;
+        @apply border-gray-900;
+        @apply px-4 p-2;
+
+        .title {
+          @apply text-2xl;
+        }
+
+        .buttons {
+          @apply flex;
+          @apply gap-4;
+        }
+      }
+
+      &-body {
+        @apply flex flex-col;
+        @apply flex-grow;
+        @apply p-4;
+        @apply gap-4;
+      }
+    }
   }
 }
 </style>
--- pages/console/writing.vue
+++ pages/console/writing.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  layout: 'user',
+  layout: 'console',
 }
 </script>
 
