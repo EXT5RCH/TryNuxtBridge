10479508e8dba13aa65839611874f90a4696bf61
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-08-21 10:23:18 +0900

    feat: add `tailwind@v3`

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,8 @@ export default defineNuxtConfig({
     postcss: {
       plugins: {
         'postcss-nested': {},
+        tailwindcss: {},
+        autoprefixer: {},
       },
     },
   },
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}', './nuxt.config.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
